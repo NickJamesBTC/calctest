fbee4287b78fc84caaaf3673b64027b8b289305b
Nicholas Higgins <nicholas@homelight.com>
2020-09-19 01:18:38 -0700

    calc v5

--- calculator.rb
+++ calculator.rb
@@ -1,34 +1,35 @@
-class Calculator
-  def get_values(x)
-    1.upto(x).map { |x|
-      puts "Value ##{x}: "
-      gets.chomp.to_i
-    }
-  end
+def get_values(x)
+  1.upto(x).map { |x|
+    puts "Value ##{x}: "
+    gets.chomp.to_f
+  }
+end
 
-  def operation
-    %w(add subtract multiply divide)
-  end
+def operation
+  %w(add subtract multiply divide exponent)
+end
 
-  def calc
-    loop do
-      puts "Would you like to #{operation.join(', ')} or quit?"
-      response = gets.chomp
-      puts "What are you trying to #{response}"
-        if response == operation[0] then
-          operator = :+
-        elsif response == operation[1] then
-          operator = :-
-        elsif response == operation[2] then
-          operator = :*
-        elsif response == operation[3] then
-          operator = :/
-        else break
-        end
-      puts "How many values?"
-      num_of_values = gets.to_i
-      sum = get_values(num_of_values).inject(operator)
-      puts "The total is #{sum.to_f}"
+def calc
+  loop do
+  puts "\nWould you like to #{operation.join(', ')} or quit?"
+  response = gets.chomp
+  puts "What are you trying to #{response}"
+    if response == operation[0] then
+      operator = :+
+    elsif response == operation[1] then
+      operator = :-
+    elsif response == operation[2] then
+      operator = :*
+    elsif response == operation[3] then
+      operator = :/
+    elsif response == operation[4] then
+      operator = :**
+    else
+      break
     end
+  puts "How many values?"
+  num_of_values = gets.to_i
+  sum = get_values(num_of_values).inject(operator)
+  puts "The total is #{sum}"
   end
 end
